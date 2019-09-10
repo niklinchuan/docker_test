579a36235a3e303d24523b60dc61b6d6abc9bb92
Emil Nikander <emil.nikander@netlight.com>
2019-09-10 14:51:37 +0200

    first commit test

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DOTNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var number in GenerateWithYield())
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+
+        public static IEnumerable<int> GenerateWithoutYield()
+        {
+            var i = 0;
+            var list = new List<int>();
+            while (i < 5)
+                list.Add(++i);
+            return list;
+        }
+
+        public static IEnumerable<int> GenerateWithYield()
+        {
+            var i = 0;
+            while (i < 5)
+                yield return ++i;
+        }
+    }
+}
---
+++ yield.cs
@@ -0,0 +1,20 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+// public class Program
+// {
+//   public static void Main()
+//   {
+//     foreach(var number in GetNumbers()) 
+//     {
+//       Console.WriteLine(number);
+//     } 
+//   }
+
+//   public static IEnumerable<int> GetNumbers() { 
+//     yield return 1; 
+//     yield return 2; 
+//     yield return 3; 
+//   }
+// }
